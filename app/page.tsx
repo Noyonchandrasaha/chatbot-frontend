40e8e7e1905a1b3ed28b67e7cce05c517cabd360
Noyonchandrasaha <noyonsaha001@gmail.com>
2026-04-20 21:55:34 +0600

    chore: design the chatbot initate state

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,26 @@ import { Button } from "@/components/ui/button"
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
-      </div>
+    <div>
+      <h1 className="text-3xl text-center font-bold py-5">Home Page</h1>
+      <Button className="fixed rounded-full bottom-5 right-5 bg-amber-300 z-50 group hover:bg-amber-500 transition">
+        <span className="absolute inset-0 bg-red-300 rounded-full opacity-70 animate-ping"></span>
+        
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-6 h-6 text-white"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M8 10h.01M12 10h.01M16 10h.01M21 12c0 4.418-4.03 8-9 8a9.77 9.77 0 01-4-.8L3 20l1.2-3.2A7.7 7.7 0 013 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+            />
+          </svg>
+      </Button>
     </div>
   )
 }
